757e205b48937203e5182d151f646f173f43b725
Aaron545 <avalladaresazuaje@byupathway.edu>
2025-05-19 16:25:08 -0400

    C# Exercise 3 Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 101);
+
+        string numberIsCorrect = "No";
+
+        while (numberIsCorrect == "No")
+        {
+            Console.Write("What is your guest? ");
+            string userGuess = Console.ReadLine();
+
+            int userGuessConverted = int.Parse(userGuess);
+
+            if (userGuessConverted == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                numberIsCorrect = "Yes";
+            }
+            else if (userGuessConverted > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                numberIsCorrect = "No";
+            }
+            else if (userGuessConverted < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                numberIsCorrect = "No";
+            }
+        }
+
+
     }
 }
