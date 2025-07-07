f5f17ac3b2acd1e9230b30dbcd34cee0bfdf5ca8
Aaron545 <avalladaresazuaje@byupathway.edu>
2025-07-07 16:29:51 -0400

    Week 2 abstraction learning activity completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2023;
+        job1._endYear = 2025;
+        
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2021;
+        job2._endYear = 2025;
+        
+        Resume resume1 = new Resume();
+        resume1._name = "John Doe";
+        resume1.Add(job1);
+        resume1.Add(job2);
+        
+        resume1.DisplayResume();
     }
 }
